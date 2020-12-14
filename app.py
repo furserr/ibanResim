ae83cd73ee751a7c9c41a0223b3f1ea09445589f
fur ser <furkann.serdaroglu@gmail.com>
2020-12-14 17:28:42 +0300

    open cv

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 import pytesseract
 from PIL import ImageFilter
 from PIL import Image
+import cv2
 
 import sys
 try:
@@ -26,8 +27,10 @@ def submitImage():
     text = ''
     filename = secure_filename(image.filename)
     image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    img = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    text = pytesseract.image_to_string(img)
+    img = c2.imread(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    text = pytesseract.image_to_string(thresh)
     f = open(os.path.join(app.config['UPLOAD_FOLDER'], filename)+'.txt','w')
     f.write(text)
     f.close()
