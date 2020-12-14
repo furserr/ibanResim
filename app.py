e2c1679320f044c5d015ae503ee3e91a834bbb79
fur ser <furkann.serdaroglu@gmail.com>
2020-12-14 17:33:36 +0300

    bug

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def submitImage():
     text = ''
     filename = secure_filename(image.filename)
     image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    img = c2.imread(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    img = cv2.imread(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
     text = pytesseract.image_to_string(thresh)
